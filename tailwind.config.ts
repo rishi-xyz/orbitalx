1c32ee8787d43295feb6a7bbd3589dee64474262
rishi-xyz <itsranahrishikesh@gmail.com>
2024-10-20 21:32:05 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,24 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "20px",
+        lg: "80px",
+      },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1200px",
       },
     },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    extend: {},
   },
   plugins: [],
 };
